e41a53cb5e1bfdb1b5a25ebedb03b25bb54162b8
simon <simon.hobbs@electrooptical.net>
2023-03-04 10:50:03 -0500

    initial commit

---
+++ part-hack.py
@@ -0,0 +1,109 @@
+'''
+Run using AppImage python:
+~/.FreeCAD/squashfs-root/usr/bin/python
+
+First add the location of FreeCAD.so to the PYTHONPATH: PYTHONPATH=${PYTHONPATH}:.FreeCAD/squashfs-root/usr/lib
+
+
++ Extracting app-image: ~/bin/freecad --appimage-extract
++ https://stackoverflow.com/questions/67325416/how-to-embed-freecad-in-a-python-virtual-environment
+
++ Paths that are separated like lower case is need to be seperated. Enter the path group, cut and paste the path
++ Intersection parts need to have their paths combined. Select the two parts in inkscape and make a union.
+
++ To use the parts export all as a step file
++ to use as an extrusion set the depth to the desired level and use the embed tool in freecad
++ to use as an indent set some large pad length, place at the desired depth, and use the Part->cut tool
+'''
+
+
+import click
+import FreeCAD
+import BOPTools.SplitFeatures
+import Draft
+
+
+def parts_intersect(parta, partb):
+  '''
+  This is hacky and depends on the text being exported as a single path and read in FreeCAD as multiple paths
+
+  Letters with seperate sections have to be broken into seperate paths at the SVG export.
+  '''
+  return parta.Label in partb.Label or partb.Label in parta.Label
+
+
+def is_sub_path(label):
+    return label[-3:-1] == "00"
+
+
+def get_base_name(label):
+    if is_sub_path(label):
+        return label[:-3]
+    return label
+
+
+def main(fname, outfname, pad_length):
+    FreeCAD.loadFile(fname) # the SVG is now the active document
+    doc = FreeCAD.activeDocument()
+
+    '''
+    cycle through objects in doc, if two object intersect take the xor of them. Since we're doing text we only do this once.
+    Sort parts by label, assume only 2 sections per part.
+    '''
+
+    base_parts = dict()
+
+    for path in doc.Objects:
+        base = get_base_name(path.Label)
+        if base not in base_parts:
+            base_parts[base] = []
+        base_parts[base].append(path)
+
+    processed_objects = []
+
+    for key, value in base_parts.items():
+        print(key, value)
+        if len(value) > 1:
+            j = BOPTools.SplitFeatures.makeXOR(name='XOR')
+            j.Objects = value
+            j.Proxy.execute(j)
+            processed_objects.append(j)
+        else:
+            processed_objects.extend(value)
+
+
+    '''
+    Make each object a sketch
+    Make sketches into parts
+    '''
+
+    parts = []
+    sketches = []
+    for part in processed_objects:
+        sketch = Draft.make_sketch(part, autoconstraints=True)
+        sketches.append(sketch)
+        name = f'Body_{sketch.Label}'
+        body = doc.addObject('PartDesign::Body', name)
+        # print(body, body.Label, name, doc.getObjectsByLabel(name))
+        # doc.recompute()
+        body.Group = [sketch]
+        parts.append(body)
+
+        pad_name = f'Pad_{body.Label}'
+        pad = body.newObject('PartDesign::Pad', pad_name)
+        # pad = doc.getObject(pad_name)
+        pad.Profile = sketch
+        pad.Length=pad_length
+        doc.recompute()
+
+
+    j = BOPTools.SplitFeatures.makeXOR(name='XOR')
+    j.Objects = parts
+    j.Proxy.execute(j)
+
+    doc.saveAs(outfname)
+
+
+if __name__ == "__main__":
+    svgfname = "/home/simon/top-label-export-path.svg"
+    main(fname=svgfname, outfname="fout.FCStd", pad_length=10)
